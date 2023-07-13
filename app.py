ae31e8a7163b1648fc64b82fd273c90790cab0d5
yash013 <yashsuthar013@gmail.com>
2023-07-13 16:34:33 +0530

    preprocessing data with streamlit

--- app.py
+++ app.py
@@ -1,3 +1,25 @@
 import streamlit as st
+import prepocessor
 
 st.sidebar.title("Whatsapp Chat Analyzer")
+
+uploaded_file = st.sidebar.file_uploader("Choose a file")
+
+if uploaded_file is not None:
+    bytes_data = uploaded_file.getvalue()
+    data = bytes_data.decode("utf-8") # converted byte data to the string
+    # st.text(data)
+    df = prepocessor.preprocess(data)
+    st.dataframe(df)
+    
+    #fetch unique users
+    users = df['user'].unique().tolist()
+    users.insert(0, "Overall")
+    #remove 'group_notification' from the users
+    users.remove('group_notification')
+    users.sort()
+    
+    st.sidebar.selectbox("Show analysis wrt", users)
+    
+    if st.sidebar.button("Show Analysis"):
+        pass
--- prepocessor.py
+++ prepocessor.py
@@ -1,3 +1,7 @@
+import re
+import pandas as pd
+
+# Preprocess the data
 def preprocess(data):
     pattern = '\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s-\s'
     
@@ -5,7 +9,7 @@ def preprocess(data):
     dates = re.findall(pattern, data)
     
     df = pd.DataFrame({'user_message': messages, 'message_date': dates})
-    # convert message_date type
+    
     df['message_date'] = pd.to_datetime(df['message_date'], format='%d/%m/%y, %H:%M - ')
 
     df.rename(columns={'message_date': 'date'}, inplace=True)
